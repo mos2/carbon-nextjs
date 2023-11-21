d2b980614e4dc37c30dfdb2286894db4c04828b1
Michael O'Sullivan <MichaelOSullivan@ie.ibm.com>
2023-11-21 13:37:49 +0000

    URL update.

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -35,7 +35,7 @@ const headers = [
 ];
 
 const EMPLOYEE_SERVER_URL =
-  'https://users-service-go-ucc.apps.o1-373466.cp.fyre.ibm.com';
+  'https://users-service-go-ucc.apps.o1-374097.cp.fyre.ibm.com';
 const EMPLOYEES_PATH = '/employees';
 
 function RepoPage() {
